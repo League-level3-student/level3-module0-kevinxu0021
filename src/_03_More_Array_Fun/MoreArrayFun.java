ac401c5d702343a98b9119e28623fc07b756590f
kevinxu0021 <32040253+kevinxu0021@users.noreply.github.com>
2020-07-18 13:01:24 -0700

    Worked on More Array Fun, Still haven't finished yet

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,50 +4,67 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	// 1. Create a main method to test the other methods you write.
-	String[] array = new String[5];
-	array[1] = "df";
-	array[2] = "er";
-	array[3] = "dfvg";
-	array[4] = "bvn";
-	array[5] = "mso";
-	public static void main(String[] args) {
 
-		
+	MoreArrayFun() {
+
+	}
+
+	public static void main(String[] args) {
+		String[] array = new String[5];
+		array[0] = "a";
+		array[1] = "b";
+		array[2] = "c";
+		array[3] = "d";
+		array[4] = "e";
+
+		printOther(array);
+
 	}
 
 	// 2. Write a method that takes an array of Strings and prints all the Strings
 	// in the array.
-	String[] printOrder(String[] array) {
-		System.out.println(array);
+	static String[] printOrder(String[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+
 		return array;
 	}
 
 	// 3. Write a method that takes an array of Strings and prints all the Strings
 	// in the array
 	// in reverse order.
-	String[] printReverse(String[] array) {
+	static String[] printReverse(String[] array) {
 		String[] output = new String[array.length];
 		int index = 0;
-		for (int i = array.length; i > 0; i--) {
+		for (int i = array.length - 1; i >= 0; i--) {
 			output[index] = array[i];
 			index++;
 		}
-		System.out.println(output);
+		for (int i = 0; i < output.length; i++) {
+			System.out.println(output[i]);
+		}
 		return output;
 	}
 
 	// 4. Write a method that takes an array of Strings and prints every other
 	// String in the array.
-	String[] printOther(String[] array) {
-		String[] output = new String[array.length];
+	static String[] printOther(String[] array) {
+		String[] output = new String[(array.length + 2) / 2];
 		int index = 0;
+		int a;
 		for (int i = 0; i < array.length; i++) {
-			if (i / 2 % 2 == 0) {
-				output[0] = array[i];
+			a = (i + 2);
+			if (a % 2 == 0) {
+				output[index] = array[i];
+				index++;
 			}
-			index++;
+
 		}
-		System.out.println(output);
+		for (int i = 0; i < output.length; i++) {
+			System.out.println(output[i]);
+		}
+
 		return output;
 	}
 
@@ -55,7 +72,7 @@ public class MoreArrayFun {
 	// in the array
 	// in a completely random order. Almost every run of the program should result
 	// in a different order.
-	String[] printRandom(String[] array) {
+	static String[] printRandom(String[] array) {
 		int index = 0;
 		String[] output = new String[array.length];
 		Random random = new Random();
